296eccb46af9ac5138b14c2ada479cb839c57b5d
hvalensi <161628333+hvalensi@users.noreply.github.com>
2025-11-07 11:10:20 -0500

    Update Step_04.List_Unique_TFs.R
    Updated script to accept command line arguments for input and output file names. Improved output message to reflect the output file name.

--- SC_MO_GRN_DB_SCRIPTS/Step_04.List_Unique_TFs.R
+++ SC_MO_GRN_DB_SCRIPTS/Step_04.List_Unique_TFs.R
@@ -1,8 +1,17 @@
+############################################################
+# Usage:
+#   Rscript Step_04.List_Unique_TFs.R input_txt_file output_txt_file
+# Example:
+#   Rscript Step_04.List_Unique_TFs.R tf_gene_symbols_unique.txt unique_tf_names.txt
+############################################################
+
 # Load necessary library
 library(data.table)
 
 # Define the input file path
-input_file <- "tf_gene_symbols_unique.txt"
+args <- commandArgs(trailingOnly = TRUE)
+input_file <- args[1]
+output_file <- args[2]
 
 # Read the unique text file into a data table
 data <- fread(input_file, header = FALSE, sep = "\t")
@@ -10,11 +19,12 @@ data <- fread(input_file, header = FALSE, sep = "\t")
 # Extract unique values from the first column (TF names)
 unique_tf_names <- unique(data[[1]])
 
-# Print the unique TF names
-print(unique_tf_names)
-
 # Write the unique TF names to a separate file
-write.table(unique_tf_names, file = "unique_tf_names.txt", quote = FALSE, sep = "\t", row.names = FALSE, col.names = FALSE)
+write.table(unique_tf_names,
+            file = output_file,
+            quote = FALSE,
+            sep = "\t",
+            row.names = FALSE,
+            col.names = FALSE)
 
-cat("Unique TF names file created: unique_tf_names.txt\n")
-
+cat("Unique TF names file created:", output_file, "\n")
