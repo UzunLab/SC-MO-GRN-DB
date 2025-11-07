9a8ad382fd2da68f086200fd7b1dad424f6424d9
hvalensi <161628333+hvalensi@users.noreply.github.com>
2025-11-07 11:05:10 -0500

    Update Step_02.Link_TFs_to_Targets.R

--- SC_MO_GRN_DB_SCRIPTS/Step_02.Link_TFs_to_Targets.R
+++ SC_MO_GRN_DB_SCRIPTS/Step_02.Link_TFs_to_Targets.R
@@ -1,8 +1,17 @@
+#############################################################
+# Usage:
+#   Rscript Step_02.Link_TFs_to_Targets.R input_csv_file output_txt_file
+# Example:
+#   Rscript Step_02.Link_TFs_to_Targets.R combined_nearest_tss_results.csv tf_gene_symbols.txt
+#############################################################
+
 # Load necessary library
 library(data.table)
 
 # Define the CSV file path
-csv_file <- "combined_nearest_tss_results.csv"
+args <- commandArgs(trailingOnly = TRUE)
+csv_file <- args[1]      # Input CSV
+output_file <- args[2]   # Output TXT
 
 # Read the CSV file into a data table
 data <- fread(csv_file)
@@ -11,8 +20,11 @@ data <- fread(csv_file)
 data[, TF := sub("-.*", "", file)]
 
 # Create the desired text file with TF names and gene symbols
-output_file <- "tf_gene_symbols.txt"
-write.table(data[, .(TF, symbol)], file = output_file, quote = FALSE, sep = "\t", row.names = FALSE, col.names = FALSE)
+write.table(data[, .(TF, symbol)],
+            file = output_file,
+            quote = FALSE,
+            sep = "\t",
+            row.names = FALSE,
+            col.names = FALSE)
 
 cat("Text file created:", output_file, "\n")
-
