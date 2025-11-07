ca986ab1021efdabc44452a027a97e22faa188ac
hvalensi <161628333+hvalensi@users.noreply.github.com>
2025-11-07 11:12:42 -0500

    Update Step_05.Format_Reference_Network.R
    Updated script to accept command line arguments for input and output file paths. Improved formatting of the write.table function call.

--- SC_MO_GRN_DB_SCRIPTS/Step_05.Format_Reference_Network.R
+++ SC_MO_GRN_DB_SCRIPTS/Step_05.Format_Reference_Network.R
@@ -1,9 +1,17 @@
+#############################################################
+# Usage:
+#   Rscript Step_05.Format_Reference_Network.R input_txt_file output_txt_file
+# Example:
+#   Rscript Step_05.Format_Reference_Network.R tf_gene_symbols_unique.txt tf_gene_symbols_final.txt
+#############################################################
+
 # Load necessary library
 library(data.table)
 
 # Define the input and output file paths
-input_file <- "tf_gene_symbols_unique.txt"
-output_file <- "tf_gene_symbols_final.txt"
+args <- commandArgs(trailingOnly = TRUE)
+input_file <- args[1]
+output_file <- args[2]
 
 # Read the existing text file into a data table
 data <- fread(input_file, header = FALSE, sep = "\t")
@@ -16,7 +24,11 @@ data$Relationship <- "Transcriptional_Regulation"
 data <- data[, .(Source, Target, Relationship)]
 
 # Write the modified data to a new text file
-write.table(data, file = output_file, quote = FALSE, sep = "\t", row.names = FALSE, col.names = TRUE)
+write.table(data,
+            file = output_file,
+            quote = FALSE,
+            sep = "\t",
+            row.names = FALSE,
+            col.names = TRUE)
 
 cat("Final text file created:", output_file, "\n")
-
